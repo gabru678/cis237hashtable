b98d4e9cdaea204467f66811c736c32651b55c1b
Paul Bath <paulbath7@gmail.com>
2016-04-04 19:43:48 -0400

    created and look into Dictionaries too

---
+++ cis237hashtable/HashMap.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237hashtable
+{
+    class HashMap
+    {
+        //Make a prime value to use in the hash Functio, and as the size of the array
+        private const int PRIMEVALUE = 97;
+        // Define a few arrays. one for keys, one for values
+        int[] theKeys;
+        string[] theValues;
+
+        // constructor
+        public HashMap()
+        {
+            theKeys = new int[PRIMEVALUE];
+            theValues = new string[PRIMEVALUE];
+        }
+
+        public override string ToString()
+        {
+            string returnString = "Key:  ";
+            returnString += "----------------------------";
+
+            for (int i = 0; i < theKeys.Length; i++)
+            {
+                if (theKeys[i] != 0)
+                {
+                    returnString += theKeys[i] + " => " + theValues[i] + "\n";
+                }
+            }
+            return returnString;
+        }
+
+        public string ShowArrays()
+        {
+            string returnString = "Index - key => Value \n";
+            returnString += "--------------------";
+            for (int i = 0; i < theKeys.Length; i++)
+            {
+                returnString += "[" + i + "] -" + theKeys[i] + " => " + theValues[i] + "\n";
+            }
+            return returnString;
+        }
+
+        public void Put(int key, string value)
+        {
+            // Get the index to use for storing the info from the hash funtion
+            int indexForValue = hashFuntion(key);
+            // Use the index to put the key and value int the assorted array
+            theKeys[indexForValue] = key;
+            theValues[indexForValue] = value;
+        }
+
+        public string Get(int key)
+        {
+            // get the index to use for looking up the value
+            int indexForValues = hashFuntion(key);
+            // return the information at the array location of indexforValues
+            return theValues[indexForValues];
+
+        }
+
+
+        // Hash function to take in a key and map it to a smaller number space that is between
+        // 0 and PRIMEVALUE.  this is acomplisehd by the mod function
+        private int hashFuntion(int key)
+        {
+            return key % PRIMEVALUE;
+        }
+    }
+}
--- cis237hashtable/Program.cs
+++ cis237hashtable/Program.cs
@@ -10,6 +10,48 @@ namespace cis237hashtable
     {
         static void Main(string[] args)
         {
+
+            // Dictionary<int, string> valleyNumToName = nw Dictionary<int, string>;
+
+            // Now to use te Hash Map
+
+            HashMap valleyNumToName = new HashMap();
+
+            Console.SetBufferSize(80, 1200);
+
+            Console.WriteLine("Adding some entres to the hash");
+            Console.WriteLine("Ex: Valley NumtoName.Put(12345, \"Mario Kart\" ");
+            valleyNumToName.Put(12345, "Mike Jones");
+            // add more names here
+
+            Console.WriteLine("the created Hash Table behold! \n");
+            Console.WriteLine(valleyNumToName.ToString());
+
+            Console.WriteLine("The full arrays as they got filled out");
+            Console.WriteLine(valleyNumToName.ShowArrays());
+
+            Console.WriteLine();
+
+
+
+            Console.WriteLine(" Get a single value out of the hash table by passing in the key");
+            Console.WriteLine("valleyNumToName.Get(12345)");
+            Console.WriteLine(valleyNumToName.Get(12345));
+
+            Console.WriteLine("\n Press enter to continue");
+            Console.ReadLine();
+
+
+            Console.WriteLine("\n What is we add 2 entries to the hash table that map to the same location o_O?");
+            Console.WriteLine("valleyNumToName.Put(26189, \"First Entry\" ))");
+            valleyNumToName.Put(26189, "Luigi Mansion");
+            Console.WriteLine("valleyNumToName.Put(25698,\"Peach Kart\")");
+            valleyNumToName.Put(25698, "Peach Kart");
+
+            Console.WriteLine("repritnting");
+            Console.WriteLine(valleyNumToName.ToString());
+            Console.WriteLine(valleyNumToName.ShowArrays());
+
         }
     }
 }
